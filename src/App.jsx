ac6b508e0a9c8fae48ac927ff59f33a2ab7b6228
Fabricio Marques <fabricio@mundialeditora.com>
2022-10-03 12:22:11 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './assets/css/global.css'
+import styles from './App.module.css'
 import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
 
   return (
     <>
       <Header/>
-      <h1>Hello 👽</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Fabricio Marques"
+            content="Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor." />
+        </main>
+      </div>
     </>
   )
 }
