cad4b1961fd1ef99e6ed751bda3e32651e80f9ed
Fabricio Marques <fabricio@mundialeditora.com>
2022-10-02 23:20:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/styles.css'
+import { Header } from './components/Header'
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1>Hello 👽</h1>
+    </>
+  )
+}
